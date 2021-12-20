ecec8a9aa7eb56741a7c3a377817c0c307a4fd3e
Reg-Sherman <reginaxsherman@gmail.com>
2021-12-20 15:05:50 -0600

    Completed step 2

---
+++ app.js
@@ -0,0 +1,5 @@
+let input = document.getElementById("color-field");
+let setColor = document.getElementById("set-color");
+let brush = document.getElementsByClassName("brush");
+let body = document.getElementsByTagName("body");
+

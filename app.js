092416e33749278ca01210f992ec1675fb50f787
Reg-Sherman <reginaxsherman@gmail.com>
2021-12-20 15:24:42 -0600

    completed step 5

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ let setColor = document.getElementById("set-color");
 let brush = document.getElementsByClassName("brush");
 let body = document.getElementsByTagName("body");
 
+setColor.addEventListener('click', () => {
+    brush.style.backgroundColor = input.value
+});
+
+for (let i = 0; i < 8000; i++) {
+    let div = document.createElement('div')
+};

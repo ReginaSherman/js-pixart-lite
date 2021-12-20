c8412284fba0efeb33c51d07747f6929f33ae8fc
Reg-Sherman <reginaxsherman@gmail.com>
2021-12-20 16:16:38 -0600

    Completed step 10

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-let input = document.getElementById("color-field");
-let setColor = document.getElementById("set-color");
-let brush = document.getElementsByClassName("brush");
-let body = document.getElementsByTagName("body");
+let input = document.querySelector("#color-field");
+let setColor = document.querySelector("#set-color");
+let brush = document.querySelector(".brush");
+let body = document.querySelector("body");
 
 setColor.addEventListener('click', () => {
     brush.style.backgroundColor = input.value
@@ -9,4 +9,11 @@ setColor.addEventListener('click', () => {
 
 for (let i = 0; i < 8000; i++) {
     let div = document.createElement('div')
+    div.classList.add('square')
+    body.appendChild(div)
+
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = brush.style.backgroundColor
+    })
 };
+
